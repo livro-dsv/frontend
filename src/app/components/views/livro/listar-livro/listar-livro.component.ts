2fdbc24b14d75b7f12e5a5cd9f9f77eeb4fd3138
Thaina Biudes <thaina.biudes@gmail.com>
2021-09-27 19:34:06 -0300

    feat: cria componente listar-livro

---
+++ src/app/components/views/livro/listar-livro/listar-livro.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from "@angular/core";
+import { Livro } from "src/app/models/livro";
+import { LivroService } from "src/app/services/livro.service";
+
+@Component({
+  selector: "app-listar-livro",
+  templateUrl: "./listar-livro.component.html",
+  styleUrls: ["./listar-livro.component.css"],
+})
+export class ListarLivroComponent implements OnInit {
+  livros: Livro[] = [];
+  constructor(private service: LivroService) {}
+
+  ngOnInit(): void {
+    this.service.list().subscribe((livros) => {
+      this.livros = livros;
+    });
+  }
+}
